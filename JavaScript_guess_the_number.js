463568bfde45da63690fe40b41eff868d65d2c9f
Ann <59180059+AnnaPelipas@users.noreply.github.com>
2020-02-02 21:41:13 +0200

    adding files

---
+++ JavaScript_guess_the_number.js
@@ -0,0 +1,17 @@
+﻿function getRandom() {
+    return Math.floor(Math.random() * 100);
+}
+let number = getRandom();
+let userNumber = Number(prompt("введите число от 0 до 100"));
+
+
+
+while (number != userNumber) {
+if (number > userNumber) {
+    alert("ваше число меньше загаданного!");
+    } else {
+        alert('ваше число больше загаданного!');
+    }
+    userNumber = prompt("введите число от 0 до 100");
+}
+alert("поздравляю, вы угадали!");
